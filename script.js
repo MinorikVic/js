4bc9fabcd649a7f6696e0f2c6d908f3d882e6b1f
MinorikVic <pobeda1105.94@gmail.com>
2020-06-26 22:25:00 +0300

    ааа

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite (n);
+};
+
 function firstGame() {
     let randomNum = Math.floor(Math.random() * 100 + 1);
 
@@ -7,16 +11,21 @@ function firstGame() {
         let game = +prompt('Угадай число от 1 до 100');
         console.log(game);
 
-        if (game >= randomNum) {
+        if (game > randomNum) {
             alert('Загаданное число меньше');
             secondGeme();
-        } else if (game <= randomNum) {
+        } else if (game < randomNum) {
             alert('Загаданное число больше');
             secondGeme();
         } else if (game === randomNum) {
             alert('Угадали!!!');
             return;
-        }
+        } else if (game === 0) {
+            alert("Вы проиграли");
+           return;
+        } else if (typeof(game === "string")) {
+            alert('Цифрами пожалуйста');
+        } 
     }
 
     secondGeme();
